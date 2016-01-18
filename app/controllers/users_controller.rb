114ecca6bffe02a23f24f847cccf59b0a047d71a
toyhammered <daniel@rassiner.com>
2016-01-17 18:44:23 -0800

    added freindship status detection, really wonky will change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @requests_from = current_user.pending_friend_requests_from.map(&:user)
+    @friends = current_user.active_friends
   end
 
   def show
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+
+  def profile_friendship_button(user)
+    case current_user.friendship_status(user)
+    when "friends"
+      "Remove Friend"
+    when "pending"
+      "Cancel Request"
+    when "requested"
+      "Accept or Deny"
+    when "not_friends"
+      "Add as Friend"
+    end
+  end
+
 end
