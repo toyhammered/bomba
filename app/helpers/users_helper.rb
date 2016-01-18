5be146958df28a332f0cd1824f170e51a3fb5a03
toyhammered <daniel@rassiner.com>
2016-01-17 18:50:57 -0800

    added notes to code relating to profile_friendship_button

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,7 @@ module UsersHelper
 
   def profile_friendship_button(user)
     case current_user.friendship_status(user)
+      # add content tags and links for this part
     when "friends"
       "Remove Friend"
     when "pending"
