0d5215020037c2acd22b7fa2f029232e284124e5
toyhammered <daniel@rassiner.com>
2016-02-02 17:24:12 -0800

    v1.1 - added activity feed

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,8 @@
 class ActivitiesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @active_friends = current_user.active_friends.includes(:posts)
+    @activities = Activity.first(5)
   end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,6 @@
 class Activity < ActiveRecord::Base
   belongs_to :user
   belongs_to :trackable, polymorphic: true
+  default_scope { order(created_at: :desc)}
+
 end
