e095b68aa658c7f347cb27770d65c882eac984d7
toyhammered <daniel@rassiner.com>
2016-02-24 15:59:09 -0800

    fixed vote serializer and added s

--- app/serializers/api/v1/vote_serializer.rb
+++ app/serializers/api/v1/vote_serializer.rb
@@ -1,4 +1,4 @@
 class Api::V1::VoteSerializer < ActiveModel::Serializer
-  attribute :id, :vote_flag, :vote_weight, :up_votes, :down_votes, :total_votes
+  attributes :id, :vote_flag, :vote_weight, :up_votes, :down_votes, :total_votes
 
 end
